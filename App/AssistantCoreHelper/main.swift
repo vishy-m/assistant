9b99fc9966f05bf833235969ce0264e177b123d3
Vishruth <vishruth.nm@gmail.com>
2026-05-14 00:51:36 -0500

    feat: add xcodegen project spec with UI + daemon targets

---
+++ App/AssistantCoreHelper/main.swift
@@ -0,0 +1,4 @@
+import Foundation
+// Replaced in Task 8 with a real NSXPCListener.
+print("AssistantCoreHelper placeholder")
+exit(0)
---
+++ App/AssistantUI/AssistantUIApp.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+@main
+struct AssistantUIApp: App {
+    var body: some Scene {
+        Settings { EmptyView() }
+    }
+}
