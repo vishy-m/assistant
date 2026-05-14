913dfce81f8c6f7d9edd9f168aedd3f1a05fca78
Vishruth <vishruth.nm@gmail.com>
2026-05-14 08:53:20 -0500

    feat: add GRDB dependency and AssistantStore SPM target

--- Package.swift
+++ Package.swift
@@ -5,10 +5,25 @@ let package = Package(
     name: "AssistantShared",
     platforms: [.macOS(.v13)],
     products: [
-        .library(name: "AssistantShared", targets: ["AssistantShared"])
+        .library(name: "AssistantShared", targets: ["AssistantShared"]),
+        .library(name: "AssistantStore", targets: ["AssistantStore"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.29.0")
     ],
     targets: [
         .target(name: "AssistantShared"),
-        .testTarget(name: "AssistantSharedTests", dependencies: ["AssistantShared"])
+        .target(
+            name: "AssistantStore",
+            dependencies: [
+                "AssistantShared",
+                .product(name: "GRDB", package: "GRDB.swift")
+            ]
+        ),
+        .testTarget(name: "AssistantSharedTests", dependencies: ["AssistantShared"]),
+        .testTarget(
+            name: "AssistantStoreTests",
+            dependencies: ["AssistantStore", "AssistantShared"]
+        )
     ]
 )
